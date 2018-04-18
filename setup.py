afe8209a6498e9c68b6cdb2a879a7a212e0e9c6d
jarbasal <jarbasai@mailfence.com>
2018-04-18 16:59:46 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py_erowid',
+    version='0.1',
+    packages=['pyerowid'],
+    url='https://github.com/JarbasAl/py_erowid',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unnoficial erowid api',
+    install_requires=["lxml", "bs4"]
+)
